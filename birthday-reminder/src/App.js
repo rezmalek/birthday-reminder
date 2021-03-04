cdc324480238698f92bc5dccccc471d186141a2b
RezMalek <rezmalek7@gmail.com>
2021-03-03 16:32:00 -0800

    create list component

--- birthday-reminder/src/App.js
+++ birthday-reminder/src/App.js
@@ -1,9 +1,17 @@
+import React, { useState } from 'react';
+import data from './data';
+import List from './List';
 
 function App() {
+  const [people, setPeople] = useState(data);
   return (
-    <div className="App">
-      <h2>Birthday Reminder</h2>
-    </div>
+    <main>
+      <section className='container'>
+        <h3>{people.length} birthdays today</h3>
+        <List people={people} />
+        <button onClick={() => console.log('clicked!')}>clear all</button>
+      </section>
+    </main>
   );
 }
 
